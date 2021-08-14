ec74c3e36e43ef43c86440d0d7669e762c1fc9cc
Nahom Abebe <58399654+nahom-hub@users.noreply.github.com>
2021-08-14 13:53:45 -0500

    Comment added for each Junit test

--- src/test/java/com/revature/P3AuthServiceApplicationTests.java
+++ src/test/java/com/revature/P3AuthServiceApplicationTests.java
@@ -1,6 +1,6 @@
 package com.revature;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -20,24 +20,29 @@ class P3AuthServiceApplicationTests {
 	void contextLoads() {
 	}
 	
+	//Test method for findAll method
 	//@Test
    public void  findAllTest() {	
 	List<User> users = us.findAll();		
 	assertTrue(true);
 	}
 	
+ //Test method for save method
+   @Test
    public void  saveTest() {	
 		List<User> users = us.findAll();		
 		assertTrue(true);
 		}
    
    
+ //Test method for findOne method
 	@Test
 	public void  findOneTest() {
 		//User u = us.save(user);
 		assertTrue(true);
 	}
 	
+	//Test method for login method
 	@Test
 	public void  loginTest() {
 		//User userLogin = us.login(user);
