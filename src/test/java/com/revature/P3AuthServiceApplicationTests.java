a1260774127e11f44829db809f2cdb724129b462
Nahom Abebe <58399654+nahom-hub@users.noreply.github.com>
2021-08-14 13:43:01 -0500

    # WARNING: head commit changed in the meantime
    comment added

--- src/test/java/com/revature/P3AuthServiceApplicationTests.java
+++ src/test/java/com/revature/P3AuthServiceApplicationTests.java
@@ -1,13 +1,50 @@
 package com.revature;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.revature.models.User;
+import com.revature.services.UserService;
 
 @SpringBootTest
 class P3AuthServiceApplicationTests {
 
+	 UserService us = new  UserService();
+	
 	@Test
 	void contextLoads() {
 	}
+	
+	//@Test
+   public void  findAllTest() {	
+	List<User> users = us.findAll();		
+	assertTrue(true);
+	}
+	
+   public void  saveTest() {	
+		List<User> users = us.findAll();		
+		assertTrue(true);
+		}
+   
+   
+	@Test
+	public void  findOneTest() {
+		//User u = us.save(user);
+		assertTrue(true);
+	}
+	
+	@Test
+	public void  loginTest() {
+		//User userLogin = us.login(user);
+		assertTrue(true);
+	}
+	
+	
 
+	
 }
